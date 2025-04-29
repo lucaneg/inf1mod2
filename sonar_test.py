f4c4df305a422095e6a0f7934128f5daeb06dd99
Luca Negrini <luca.negrinii@gmail.com>
2025-04-29 11:45:11 +0200

    Create sonar_test.py

---
+++ sonar_test.py
@@ -0,0 +1,24 @@
+class MyTestClass:
+  # ...
+  def __iadd__(self, other, individual):
+    # ...
+    pass
+  
+  def __str_(self):
+    # ...
+    pass
+    
+  def some_function():
+    pass
+    
+l = [5, 6, 7]
+l()
+x = 14
+if x == l:
+  print(x)
+if x == 0.00000001:
+  print('equals')
+  
+def my_pow(num, exponent): 
+    num = num * my_pow(num, exponent - 1)
+    return num
